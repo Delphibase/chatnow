65383ab3b604b144754e41f35da6ae69e9b35c43
Dirk Wetzels <dirk.wetzels@sykosch.com>
2022-10-02 22:00:40 +0200

    Initial: HTTP-Server mit Express+Socket-IO erstellen

--- server/applicationserver.js
+++ server/applicationserver.js
@@ -1,9 +1,26 @@
+const express = require('express');
+const http = require ('http');
+const cors = require ('cors');
+const {Server} = require ('socket.io');
 const ConnectionManager = require ('./database/connectionmanager');
 
 class ApplicationServer {
 	async boot () {
-		let cm = new ConnectionManager ();
-		return cm.initDatabase ();
+    try {
+      let cm = new ConnectionManager ();
+      await cm.initDatabase ();
+      
+      const app = express ();
+      const httpServer = http.createServer (app);
+      const io = new Server (httpServer, {'cors': {'origin': 'http://localhost:3000'}});
+
+      httpServer.listen (3001, () => {
+        console.log ('Http-Server initialisiert. Warten auf Anfragen...');
+      });
+    }
+    catch (err) {
+      console.log ("Fehler beim Start des App-Servers. Server ist nicht gestartet.", err);
+    }
 	}
 }
 
